af0b01a409dd3ca5c81ea90123c7dcdcae9ce46f
Arunkumar1712 <arunkumar1712007@gmail.com>
2023-12-24 22:54:29 +0530

    Day4 taskc

---
+++ day 4 task/day 4task1_c.js
@@ -0,0 +1,40 @@
+// Program for Sum of all numbers in an array using Anonymous fuction
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+    var a=userInput[0];                    // Taking Input from the user as string in array format
+    var b=a.split(" ");                    // Splitting the input according to the space and stores as example [1,2,3,4,5]...
+    var add=function(b){                   // Declaring the variable add to store the anonymous function
+    var sum=0;                             // declaring the variable to store the sum of array value
+    for(var i=0;i<b.length;i++){           // this for loop iterate through array
+        sum+=parseInt(b[i]);               // adding the each element in the array to the sum variable
+    }
+    console.log(sum);                      // Logging the value of sum in console
+    }
+    add(b);                                // calling the function
+
+
+// Program for Sum of all numbers in an array using IIFE function
+var a=userInput[0];                        // Taking Input from the user as string in array format
+    var b=a.split(" ");                    // Splitting the input according to the space and stores as example [1,2,3,4,5]...
+(function(){                               // This function will execute without calling it
+    var sum=0;                             // declaring the variable to store the sum of array value
+    for(var i=0;i<b.length;i++){           // this for loop iterate through array
+        sum+=parseInt(b[i]);               // adding the each element in the array to the sum variable
+    }
+    console.log(sum);                      // Logging the value of sum in console
+    })()
+      
+
+});
