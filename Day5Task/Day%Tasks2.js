97a2e35a92395e6d77e0837b6e759c1c7117a3ba
Arunkumar1712 <arunkumar1712007@gmail.com>
2023-12-27 15:03:01 +0530

    day 5 task

---
+++ Day5Task/Day%Tasks2.js
@@ -0,0 +1,38 @@
+const obj={resume:{
+    name:"Arunkumar",contacts:{emailid:"arunk@GMAIL.COM",phonenumber:"9329939393",
+    linkedin:"www.linkedin.com/in/arunkumar-r-539601209"},education:["Engineering(ECE)","Hsc","SSc"],},
+    experience:{jobtitle:"Associate Sales and Service Engineer (Territory Officer)",duration:"8months",
+    skillused:"R&D,communication,CRM,marketresearch"
+    },
+    skills:"HTML,CSS,JAVA,JS"
+};
+
+//for in loop 
+
+for(const key in obj){
+    console.log(key);               // this will return the keys in the  json
+}
+
+for(const key in obj){
+    console.log(obj[key]);         // this will return the keys and ot value in the  json
+}
+
+
+// for of loop
+let key=Object.keys(obj);                  // this key stores the key alone
+for(let i of key){
+    console.log(i);                       // this will return keys in json
+}
+
+let keys1=Object.values(obj);             // this key stores the values of keys
+for(let i of keys1){
+    console.log(i);                      // this will return the keys and ot value in the  json
+}
+
+// // for each loop
+   const ke=Object.keys(obj);              // this key stores the values of keys
+   ke.forEach(key =>{
+   console.log(key,obj[key])               // this will return the keys and ot value in the  json
+   });
+   
+
---
+++ Day5Task/Day5Tasks1.js
@@ -0,0 +1,8 @@
+var obj={resume:{
+    name:"Arunkumar",contacts:{emailid:"arunk@GMAIL.COM",phonenumber:"9329939393",
+    linkedin:"www.linkedin.com/in/arunkumar-r-539601209"},education:["Engineering(ECE)","Hsc","SSc"],},
+    experience:{jobtitle:"Associate Sales and Service Engineer (Territory Officer)",duration:"8months",
+    skillused:"R&D,communication,CRM,marketresearch"
+    },
+    skills:"HTML,CSS,JAVA,JS"
+}
